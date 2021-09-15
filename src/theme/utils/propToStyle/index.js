4468f848f7917cc4b4861a5e4f61571d8b8c0c68
Milena Yamamoto <milenayamamoto@gmail.com>
2021-09-15 00:17:50 -0300

    test(new tests): adding new unit tests

---
+++ src/theme/utils/propToStyle/index.js
@@ -0,0 +1,28 @@
+import { breakpointsMedia } from '../breakpointsMedia';
+
+export function propToStyle(propName) {
+  return (props) => {
+    const propValue = props[propName];
+
+    if (typeof propValue === 'string' || typeof propValue === 'number') {
+      return {
+        // textAlign: props.textAlign
+        [propName]: propValue,
+      };
+    }
+
+    if (typeof propValue === 'object') {
+      const breakpoints = {};
+
+      if (propValue.xs) breakpoints.xs = { [propName]: propValue.xs };
+      if (propValue.sm) breakpoints.sm = { [propName]: propValue.sm };
+      if (propValue.md) breakpoints.md = { [propName]: propValue.md };
+      if (propValue.lg) breakpoints.lg = { [propName]: propValue.lg };
+      if (propValue.xl) breakpoints.xl = { [propName]: propValue.xl };
+
+      return breakpointsMedia(breakpoints);
+    }
+
+    return {};
+  };
+}
