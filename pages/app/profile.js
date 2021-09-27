62a8931324f92792a0429bdd1b899093bc213080
Milena Yamamoto <milenayamamoto@gmail.com>
2021-09-27 01:32:47 -0300

    perf(hook): adding useUserService

--- pages/app/profile.js
+++ pages/app/profile.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { authService } from '../../src/services/auth/authService';
-import { userService } from '../../src/services/user/userService';
+import { useUserService } from '../../src/services/user/hook';
 
-export default function ProfilePage(props) {
+export default function ProfilePage() {
+  const dados = useUserService.getProfilePage();
+
+  // eslint-disable-next-line no-console
+  console.log(dados);
   return (
     <div>
       Página de Profile!
-      <pre>
+      {dados.loading && 'Loading'}
+      {!dados.loading && dados.data && 'Carregou com sucesso!'}
+      {!dados.loading && dados.error}
+      {/* <pre>
         {JSON.stringify(props, null, 4)}
-      </pre>
+      </pre> */}
       <img
         src="https://media.giphy.com/media/bn0zlGb4LOyo8/giphy.gif"
         alt="Nicolas Cage"
@@ -23,14 +30,14 @@ export async function getServerSideProps(ctx) {
 
   if (hasActiveSession) {
     const session = await auth.getSession();
-    const profilePage = await userService.getProfilePage(ctx);
+    // const profilePage = await userService.getProfilePage(ctx);
     return {
       props: {
         user: {
           ...session,
-          ...profilePage.user,
+          // ...profilePage.user,
         },
-        posts: profilePage.posts,
+        // posts: profilePage.posts,
       },
     };
   }
